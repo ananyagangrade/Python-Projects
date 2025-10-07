415fe2bda5184c0a2c63319ec0204fe4258e789e
ananyagangrade <ananyagangrade@gmail.com>
2025-10-07 13:48:15 +0530

    Update QR_scanner.py

--- QR_scanner.py
+++ QR_scanner.py
@@ -2,3 +2,18 @@ import qrcode
 
 upi_id = input("Enter your UPI ID: ")
 
+phonepay_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+paytm_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+google_pay_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+
+phonepay_qr = qrcode.make(phonepay_url)
+paytm_qr = qrcode.make(paytm_url)
+google_pay_qr = qrcode.make(google_pay_url)
+
+phonepay_qr.save('phonepay_qr.png')
+paytm_qr.save('paytm_qr.png')
+google_pay_qr.save('googlepay_qr.png')
+
+phonepay_qr.show()
+paytm_qr.show()
+google_pay_qr.show()
